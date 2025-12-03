a30885cf56f3dce1a54b4fe7eb1f3f287a93928f
Monica Moreno <169068483+MonicaMoren@users.noreply.github.com>
2025-12-03 11:33:50 +0000

    03 12 2025

--- js/form.js
+++ js/form.js
@@ -7,6 +7,8 @@ event.preventDefault();
 
   //Criando a linha e as celulas da tabela do novo paciente
   var pacienteTr = montaTr(paciente);
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
 });
 
 function obtemPacienteDoFormulario(form){
@@ -20,4 +22,29 @@ function obtemPacienteDoFormulario(form){
   return paciente;
 }
 
-function montaTr(){}
+function montaTr(paciente){
+  var pacienteTr = document.createElement("tr");
+pacienteTr.classList.add("paciente");
+  pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+  pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+  return pacienteTr;
+  
+}
+
+
+function montaTd(dado, classe){
+var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+  return td;
+  
+}
+
+
+
+
+
+
